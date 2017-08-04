ef475bc2a66f160a7c439922e1246ca4494ea38d
Jorrin Pollard <me@jorrinpollard.com>
2017-08-04 00:59:43 -0700

    Ruby installation runs only when ruby2.4 does not exist.

--- provision/vagrant.sh
+++ provision/vagrant.sh
@@ -2,9 +2,11 @@
 if [ ! -d /home/vagrant/go-ipfs ]; then wget https://dist.ipfs.io/go-ipfs/v0.4.10/go-ipfs_v0.4.10_linux-amd64.tar.gz &&  tar xzvf go-ipfs_v0.4.10_linux-amd64.tar.gz && rm go-ipfs_v0.4.10_linux-amd64.tar.gz; fi
 
 # install ruby
-sudo apt-add-repository -y ppa:brightbox/ruby-ng
-sudo apt-get update
-sudo apt-get install -y ruby2.4 ruby2.4-dev
+if [[ ! -f "/usr/bin/ruby2.4" ]]; then 
+  sudo apt-add-repository -y ppa:brightbox/ruby-ng
+  sudo apt update
+  sudo apt install -y ruby2.4 ruby2.4-dev
+fi
 
 # start ipfs
 /home/vagrant/go-ipfs/ipfs init
